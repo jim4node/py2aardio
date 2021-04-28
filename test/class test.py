2c64cc5af102519a4cc4cd15ac62966e6233a817
jim4work <jim4work@qq.com>
2021-04-28 17:02:22 +0800

    first commit files

---
+++ test/class test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+ 
+#类定义
+class people:
+    #定义基本属性
+    name = ''
+    age = 0
+    #定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+    #定义构造方法
+    def __init__(self,n,a,w):
+        self.name = n
+        self.age = a
+        self.__weight = w
+    def speak(self):
+        print("%s 说: 我 %d 岁。" %(self.name,self.age))
+ 
+#单继承示例
+class student(people):
+    grade = ''
+    def __init__(self,n,a,w,g):
+        #调用父类的构函
+        people.__init__(self,n,a,w)
+        self.grade = g
+    #覆写父类的方法
+    def speak(self):
+        print("%s 说: 我 %d 岁了，我在读 %d 年级"%(self.name,self.age,self.grade))
+ 
+print "This is a demo class"
+"""
+创建类的实例,并调用类的方法
+"""
+s = student('ken', 10, 60, 3)
+s.speak()
---
+++ test/strategy.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+#         在此前提下，对本软件的使用同样需要遵守 Apache 2.0 许可。
+    # 开始
+test = 1 # jim
+a = '#1'    #test2
+b = "#2"
+
+from functools import wraps
+from rqalpha.core.events import EVENT, Event
+from rqalpha.utils.logger import user_system_log
+
+#---------父类 Test-----------#
+class Test(object):
+    # 构造函数
+    def __init__(self):
+        self.a = 0
+    # 不规范的注释缩进
+#----------------------
+        self.b = 0
+        # -很长的语句,多行
+        if ((maxp-current_price)/current_price)*100>1 \
+            or sum(hi['volume'])<0.5*hiday['volume'] \
+            or 100*(current_price-sum(hi['money'])/sum(hi['volume']))/hiday['close']>2:
+            g.security.remove(stock)
+            
+
+#--------子类 ChildTest--------
+class ChildTest(Test):
+    def __init__(self):
+        self.b = 1
+
+#--------孙类 ChildTest--------
+class SubChildTest(Test, ChildTest):
+    def __init__(self):
+        self.c = 2
+       self.d=4 # 不规范的缩进3
+         self.e=5 # 不规则的缩进5
+       
+        
+"""
+# 运行策略
+# @param func
+# @return wrapper
+"""
+def run_when_strategy_not_hold(func):
+    # test begin {{
+    if True:
+        a = 1
+    elif False :
+        a = 0
+    else:
+        a = 2
+    
+    try:
+       a = 1 + w
+    except e:
+       print(e)
+       pass
+    finally:
+       print('ok')
+    # }}test end
+    
+    from rqalpha.utils.logger import system_log
+    def wrapper(*args, **kwargs):
+        if not Environment.get_instance().config.extra.is_hold:
+            return func(*args, **kwargs)
+        else:
+            system_log.debug(_(u"not run {}({}, {}) because strategy is #hold#").format(func, args, kwargs))
+
+    return wrapper
+
+
+class Strategy(object):
+    def __init__(self, event_bus, scope, ucontext):
+        self._user_context = ucontext
+        self._current_universe = set()
+
+        self._init = scope.get('init', None)
+        self._handle_bar = scope.get('handle_bar', None)
+        self._handle_tick = scope.get('handle_tick', None)
+        self._open_auction = scope.get("open_auction", None)
+        func_before_trading = scope.get('before_trading', None)
+        if func_before_trading is not None and func_before_trading.__code__.co_argcount > 1:
+            self._before_trading = lambda context: func_before_trading(context, None)
+            user_system_log.warn(_(u"deprecated parameter[bar_dict] in before_trading function."))
+        else:
+            self._before_trading = func_before_trading
+        self._after_trading = scope.get('after_trading', None)
+
+        if True and self._before_trading is not None:
+            event_bus.add_listener(EVENT.BEFORE_TRADING, self.before_trading)
+        if False or self._handle_bar is not None:
+            event_bus.add_listener(EVENT.BAR, self.handle_bar)
+        if self._handle_tick is not None:
+            event_bus.add_listener(EVENT.TICK, self.handle_tick)
+        if self._after_trading is not None:
+            event_bus.add_listener(EVENT.AFTER_TRADING, self.after_trading)
+        if self._open_auction is not None:
+            event_bus.add_listener(EVENT.OPEN_AUCTION, self.open_auction)
+
+    @property
+    def user_context(self):
+        return self._user_context
+
+    def init(self):
+        if self._init:
+            with ExecutionContext(EXECUTION_PHASE.ON_INIT):
+                with ModifyExceptionFromType(EXC_TYPE.USER_EXC):
+                    self._init(self._user_context)
+
+        Environment.get_instance().event_bus.publish_event(Event(EVENT.POST_USER_INIT))
+
+    @run_when_strategy_not_hold
+    def before_trading(self, event):
+        with ExecutionContext(EXECUTION_PHASE.BEFORE_TRADING):
+            with ModifyExceptionFromType(EXC_TYPE.USER_EXC):
+                self._before_trading(self._user_context)
+
+    @run_when_strategy_not_hold
+    def handle_bar(self, event):
+        bar_dict = event.bar_dict
+        with ExecutionContext(EXECUTION_PHASE.ON_BAR):
+            with ModifyExceptionFromType(EXC_TYPE.USER_EXC):
+                self._handle_bar(self._user_context, bar_dict)
+
+    @run_when_strategy_not_hold
+    def open_auction(self, event):
+        bar_dict = event.bar_dict
+        with ExecutionContext(EXECUTION_PHASE.OPEN_AUCTION):
+            with ModifyExceptionFromType(EXC_TYPE.USER_EXC):
+                self._open_auction(self._user_context, bar_dict)
+
+    @run_when_strategy_not_hold
+    def handle_tick(self, event):
+        tick = event.tick
+        with ExecutionContext(EXECUTION_PHASE.ON_TICK):
+            with ModifyExceptionFromType(EXC_TYPE.USER_EXC):
+                self._handle_tick(self._user_context, tick)
+
+    @run_when_strategy_not_hold
+    def after_trading(self, event):
+        with ExecutionContext(EXECUTION_PHASE.AFTER_TRADING):
+            with ModifyExceptionFromType(EXC_TYPE.USER_EXC):
+                self._after_trading(self._user_context)
+
+    def wrap_user_event_handler(self, handler):
+        @wraps(handler)
+        def wrapped_handler(event):
+            with ExecutionContext(EXECUTION_PHASE.GLOBAL):
+                with ModifyExceptionFromType(EXC_TYPE.USER_EXC):
+                    return handler(self._user_context, event)
+        return wrapped_handler
